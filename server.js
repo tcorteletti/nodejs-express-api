779d60ce91111536cfd9a4d5bf7710bbcf0676b4
Thiago Corteletti <tcorteletti@gmail.com>
2017-07-24 09:15:26 -0300

    correção porta

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-http.createServer(app).listen(3001, function() {
-    console.log('Servidor estutando na porta: ' + this.address().port);
+var server_port = process.env.PORTA || 3000
+var server_ip_address = process.env.HOSTNAME || '127.0.0.1'
+
+http.createServer(app).listen(server_port, server_ip_address, function() {
+    console.log('Servidor ' + this.address().address + ' estutando na porta: ' + this.address().port);
 });
